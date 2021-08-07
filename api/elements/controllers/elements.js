9601081965e0cceb2254736ab36741c7a90c399f
Gabriel CM <gabriel.moura@gsw.com.br>
2021-08-07 15:50:28 -0300

    fix: creating custom route to findOne by AtomicNUmber

--- api/elements/controllers/elements.js
+++ api/elements/controllers/elements.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { number } = ctx.params;
+
+    const entity = await strapi.services.elements.findOne({ number });
+    return sanitizeEntity(entity, { model: strapi.models.elements });
+  },
+};
