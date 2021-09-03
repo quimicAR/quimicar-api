edab339487de05b4f55d77003edee4fa046bdad8
Gabriel CM <gabriel.moura@gsw.com.br>
2021-09-03 20:36:33 -0300

    feat: testing clean project inside GitPod

---
+++ ddl.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE db;
+
+CREATE USER administrator WITH PASSWORD 'password';
+
+GRANT ALL PRIVILEGES ON DATABASE "db" to administrator;
+
+CREATE TABLE elements (
+	"element_id" serial,
+	"number" numeric(9,2),
+	"name" text,
+	"atomic_mass" numeric(9,2),
+	"appearance" text,
+	"boil" numeric(9,2),
+	"category" text,
+	"density" numeric(9,2),
+	"melt" numeric(9,2),
+	"molar_heat" numeric(9,2),
+	"discovered_by" text,
+	"named_by" text,
+	"period" numeric(9,2),
+	"phase" text,
+	"symbol" text,
+	"source" text,
+	"spectral_img" text,
+	"summary" text,
+	"xpos" numeric(9,2),
+	"ypos" numeric(9,2),
+	"shells" numeric(9,2),
+	"electron_configuration" text,
+	"electron_configuration_semantic" text,
+	"electron_affinity" numeric(9,2),
+	"electronegativity_pauling" numeric(9,2),
+	"ionization_energies" numeric(9,2),
+	"element_img" text,
+	PRIMARY KEY( element_id )
+);
+
+CREATE TABLE users (
+	"user_id" serial,
+	"name" text,
+	"password" text,
+	PRIMARY KEY( user_id )
+
+)
