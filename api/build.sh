451e10c1cef4a4342f81f9f0175c8114bf309bd9
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-07 20:52:37 -0300

    fix: removing api lost folder inside project

--- api/build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf target
-./mvnw clean package -DskipTests
-# cp ./target/quimicar-api-0.0.1-SNAPSHOT.jar ./src/main/docker
-# cd src/main/docker
-docker-compose down
-docker rmi quimicar_api:latest
-docker-compose up --build -d
-docker logs --follow quimicar_api
--- api/src/main/java/br/com/quimicar/api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.quimicar.api.repository;
-
-import br.com.quimicar.api.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-    UserEntity findByEmail(String email);
-}
