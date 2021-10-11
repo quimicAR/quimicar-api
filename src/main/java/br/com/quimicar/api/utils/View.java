273ec38176240394a7524b7c1500bf029e591ba7
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-11 20:54:24 -0300

    feat: view class with View's for JsonView

---
+++ src/main/java/br/com/quimicar/api/utils/View.java
@@ -0,0 +1,8 @@
+package br.com.quimicar.api.utils;
+
+public class View {
+    public static class ElementListView {}
+    public static class ElementDetailsView {}
+    public static class PermissionView {}
+    public static class UserView {}
+}
