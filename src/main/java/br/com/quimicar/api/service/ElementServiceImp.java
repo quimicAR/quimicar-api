ab5ea0ffe208d56e18639b30d41b2b294f28c248
Gabriel CM <gabriel.moura@gsw.com.br>
2021-10-26 12:36:13 -0300

    feat: trying to deploy

--- src/main/java/br/com/quimicar/api/service/ElementServiceImp.java
+++ src/main/java/br/com/quimicar/api/service/ElementServiceImp.java
@@ -31,6 +31,7 @@ public class ElementServiceImp implements ElementService {
     }
 
     @Override
+    @PreAuthorize("permitAll()")
     public List<String> findAllCategories() {
         try {
             return Arrays.asList("Noble Gases", "Alkali Metals", "Alkaline Earth Metals",
