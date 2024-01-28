426d4f0dff02850516c000575671da6722e3fe66
Gabriel CM <gabrielcostamoura1@gmail.com>
2024-01-28 16:48:06 -0300

    ci: del target dir && run build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf target
 ./mvnw clean package -DskipTests
 
-docker-compose down
-docker rmi quimicar_api:latest
-docker-compose up --build -d
-docker logs --follow quimicar_api
+# docker-compose down
+# docker rmi quimicar_api:latest
+# docker-compose up --build -d
+# docker logs --follow quimicar_api
